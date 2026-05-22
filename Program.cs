842bd097efeb3978b8ef4d8d37bb3e82cc15a3f3
NORAELENA KARELIA URROZ LOPEZ <nkurroz@uamv.edu.ni>
2026-05-22 11:33:33 -1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,16 +22,16 @@ Console.WriteLine("=============================================================
 // Mostrar el detalle día por día
 for (int i = 0; i < 7; i++)
 {
-    Console.WriteLine($"{dias[i],-10} | Producto: {nombresProductos[i],-10} | Cant: {cantidades[i],-3} | Precio: ${precios[i],-5} | Total: ${ventasDiarias[i]}");
+    Console.WriteLine($"{dias[i],-10} | Producto: {nombresProductos[i],-10} | Cant: {cantidades[i],-3} | Precio: C${precios[i],-5} | Total: C${ventasDiarias[i]}");
 }
 
 Console.WriteLine("--------------------------------------------------------------");
-Console.WriteLine($"Total recaudado en la semana: ${totalVendidoSemana}");
+Console.WriteLine($"Total recaudado en la semana: C${totalVendidoSemana}");
 
 // Evitamos mostrar el mensaje de mayor venta si no se vendió nada en toda la semana
 if (totalVendidoSemana > 0)
 {
-    Console.WriteLine($"El día de mayor venta fue el {diaMayorVenta} con un total de ${mayorVenta}");
+    Console.WriteLine($"El día de mayor venta fue el {diaMayorVenta} con un total de C${mayorVenta}");
 }
 else
 {
