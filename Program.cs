7735e7d572d701723c10f3948045a036b6b4cefc
nkurroz <nkurroz@uamv.edu.ni>
2026-05-22 09:41:02 -0600

    feat: Agregar tarea2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Una tienda de tecnología desea registrar las ventas realizadas durante una semana.
+El sistema será desarrollado en equipos de 4 estudiantes utilizando arreglos en C#*/
+
+/*Integrante 1 — Diseño de datos
+Crear los arreglos.
+Definir variables y arreglos.*/
+
+int [] ventas = new int[7];
+int[,] productos = new int[3, 3];
+string nombreProducto = "";
+int precio = 0;
+int cantidad = 0;
+int menu = 0;
+
+/*Integrante 2 — Entrada de datos
+Programar el ingreso de información.
+Validar datos.*/
+
+/*Integrante 3 — Procesamiento
+Realizar cálculos y estadísticas.*/
+
+/*Integrante 4 — Salida y pruebas
+Mostrar reportes.
+Probar el sistema y corregir errores.*/
